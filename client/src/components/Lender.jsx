599d3512069722cc7f6be33b2195a7e45a648e4d
zoobooks <zblaise23@gmail.com>
2021-12-16 18:45:35 -0600

    added form to lender

--- client/src/components/Lender.jsx
+++ client/src/components/Lender.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Lender = () => {
+  const {register, handleSubmit} = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  }
+
   return (
     <div>
       <h2>Lender Page</h2>
+      <form onSumbit={handleSubmit(onSubmit)}>
+        <label>
+          Item For Rent
+        </label>
+        <input {...register("itemforLease")} />
+        <label>
+          Item Description
+        </label>
+        <input {...register("itemDescription")} />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 };
