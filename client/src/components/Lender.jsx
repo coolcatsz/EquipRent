e434ed51735e9fd63e52c002885cbc9a2ba8af37
zoobooks <zblaise23@gmail.com>
2021-12-19 22:44:49 -0600

    item is now added to database, still have to add images

--- client/src/components/Lender.jsx
+++ client/src/components/Lender.jsx
@@ -2,64 +2,44 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 
-const Lender = () => {
-
+const Lender = ({user}) => {
   const { register, handleSubmit } = useForm();
-  const listItem = e => {
+
+  const listItem = data => {
     // e.preventDefault();
+    // console.log(data);
     axios.post('item/newItems', {
-      brand,
-      type,
-      condition,
-      value,
-      description,
-      itemId: item.id, 
+      brand: data.brand,
+      type: data.type,
+      price: data.price,
+      condition: data.condition,
+      value: data.value,
+      availability: true,
+      description: data.description,
       userId: user.id
     }).then(() => {
       console.log('Item successfully Listed!');
-    }).catch((err) => console.error('Item listing error'));
+    }).catch((err) => {
+      console.error('Item listing error')
+    });
   }
 
-  //brand, type, price, condition, value, availability, description, itemId, userId } = req.body;
   return (
-    <div>
+    <div style={{display: 'inline-block'}}>
       <h2>Make a listing for an Item:</h2>
       <form onSubmit={handleSubmit(listItem)}>
-      
-        <label>
-        Item For Rent
-        </label>
-
-        <input {...register("brand")} />
-
-        <label>
-        Upload an image of the item
-        </label>
-        <input {...register("itemImg")} type="file"/>
-
-        <label>
-        Category
-        </label>
-        <input {...register("type")} />
-
-
-        <label>
-        Item Description
-        </label>
-
-
-        <label>
-        Accessed Value
-        </label>
-        <input {...register("value")} />
-
-        <label>
-        Item Condition
-        </label>
-        <input {...register("condition")} />
-
-
-        <input type="submit" value="submit" />
+        <div>
+          <ul>
+            <li><label>Item For Rent</label><input {...register("brand")} /></li>
+            <li><label>Category</label><input {...register("type")} /></li>
+            <li><label>Item Description</label><input {...register("description")} /></li>
+            <li><label>Rental Fee</label><input {...register("price")} /></li>
+            <li><label>Accessed Value</label><input {...register("value")} /></li>
+            <li><label>Item Condition</label><input {...register("condition")} /></li>
+            <li><label>Upload an image of the item</label><input {...register("itemImg")} type="file"/></li>
+            <input type="submit" value="submit" />
+          </ul>
+        </div>
       </form>
     </div>
   );
