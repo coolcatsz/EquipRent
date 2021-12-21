53a6eafed6760d9d5fb550b08467fb844ea25f35
csherpa <chhoti0712@gmail.com>
2021-12-21 17:17:31 -0600

    implement Reservation.Create helper func, set up post route and set up basic calender component

--- server/helpers/reservationHelper.js
+++ server/helpers/reservationHelper.js
@@ -1,1 +1,10 @@
 const { db } = require('#db/index.js');
+
+const newReserve = (reservation) => Reservation.create({
+  startDate: reservation.startDate,
+  endDate: reservation.endDate,
+  price: reservation.price,
+  total: reservation.total
+});
+
+module.exports.newReserve = newReserve;
--- server/routes/reservation-router.js
+++ server/routes/reservation-router.js
@@ -1,4 +1,9 @@
 const { Router } = require('express');
 const reserveRoute = Router();
+const {newReserve} = require('../helpers/reservationHelper');
+
+reserveRoute.post('/insertReserve', (req, res) => {
+  console.log(req.body);
+});
 
 module.exports = reserveRoute;
