b880f667183abc26449bf8404a51eb7d4b0698c9
Amelia Neville <muzikgirl12@gmail.com>
2022-01-02 23:02:03 -0600

    added logo, when clicked, redirects to homepage

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
         use: ['babel-loader'],
       },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
