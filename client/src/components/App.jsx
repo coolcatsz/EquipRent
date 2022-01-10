adc702a4743cd7a0b57e0976a007faa0130e1cab
t-suarez <tsuare1@gmail.com>
2022-01-10 03:10:36 -0600

    getting it mostly working

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,6 +9,7 @@ import Login from './Login.jsx';
 import ItemList from './ItemList.jsx';
 import SingleItem from './SingleItem.jsx';
 import BookmarkList from './BookmarkList.jsx';
+import Map from './Map.jsx';
 import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -24,6 +25,8 @@ const App = () => {
   const [currentItem, setCurrentItem] = useState({});
   const [allUsers, setAllUsers] = useState([]);
   const [person, setPerson] = useState({});
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
 
   const authUser = () => {
     axios.get('/auth/verify')
@@ -59,7 +62,7 @@ const App = () => {
     setPerson(person);
   };
 
-  const notify = (data) => toast(`User ID #${data.user_id} received a reservation!`, {
+  const notify = (data) => toast('You\'ve made a reservation!', {
     position: 'top-right',
     autoClose: 5000,
     hideProgressBar: false,
@@ -82,10 +85,17 @@ const App = () => {
     });
     socket.on('update', data => {
       notify(JSON.parse(data.message.payload));
-      console.log('DATA RECEIVED notify FROM DB: ', data.message.payload);
     });
 
   }, []);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      console.log('latitude: ', lat, 'longitude: ', long);
+    });
+  }, [lat, long]);
 
   return (
 
@@ -102,6 +112,7 @@ const App = () => {
               <Route exact path ='/' element={<ItemList itemList={itemList} handleClick={oneItem} user={user} addBookmark={addBookmark} />}/>
               <Route exact path ='/item/:itemId' element={ <SingleItem user={user} currentItem={currentItem} addBookmark={addBookmark} appUser={allUsers} userClick={oneUser}/> } />
               <Route exact path ='/bookmark' element={ <BookmarkList user={user} itemList={itemList} currentItem={currentItem} /> } />
+              <Route exact path ='/map' element={<Map lat={lat} long={long} itemList={itemList} setCurrentItem={setCurrentItem}/>}/>
             </Routes>
           </div>
         ) : (
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
 module.exports = {
   BASEURL: {
     url: 'http://localhost:3000'
+  },
+  GOOGLE_MAPS_API: {
+    KEY: 'AIzaSyDG1iQtU5t3d5z8w3eeGiixImK7aLQEDSg'
   }
 };
