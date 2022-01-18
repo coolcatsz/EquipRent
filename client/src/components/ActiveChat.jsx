9f7207a607882b2a1b2475061baadc7bb1ae6f2e
t-suarez <tsuare1@gmail.com>
2022-01-18 13:55:03 -0600

    added paper tag and div class Chat to ActiveChat for css styling

--- client/src/components/ActiveChat.jsx
+++ client/src/components/ActiveChat.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import ScrollToBottom from 'react-scroll-to-bottom';
+import Paper from '@material-ui/core/Paper';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3001');
@@ -38,48 +39,52 @@ const ActiveChat = ({ googleUser}) => {
     socket.emit('join_room', room, googleUser.username);
 
     socket.on('receive_message', (data) => {
-      console.log('received message, data: ', data);
+      // console.log('received message, data: ', data);
       setMessageList((list) => [...list, data]);
     });
   }, [socket]);
 
   return (
-    <div className='chat-window'>
-      <div className='chat-header'>
-        <p>{`Item: ${room}`}</p>
+    <Paper>
+      <div className="Chat">
+        <div className='chat-window'>
+          <div className='chat-header'>
+            <p>{`Item: ${room}`}</p>
+          </div>
+          <div className='chat-body'>
+            <ScrollToBottom className='message-container'>
+              {
+                messageList.map((messageBody, i) => {
+                  return <div key={i} className='message' id={googleUser.username === messageBody.author ? 'you' : 'other'}>
+                    <div>
+                      <div className='message-meta'>
+                        <img width={'100%'} src={`${messageData.thumbnail}`} alt="profile pic" />
+                        <p id='author'>{messageBody.author}</p>
+                      </div>
+                      <div className='message-content'>
+                        <p>{messageBody.message}</p>
+                        {/* <p id='time'>{messageBody.time}</p> */}
+                      </div>
+                    </div>
+                  </div>;
+                })
+              }
+            </ScrollToBottom>
+          </div>
+          <div className='chat-footer'>
+            <input
+              type="text"
+              value={currentMessage}
+              placeholder='Type Message'
+              onChange={(event) => setCurrentMessage(event.target.value)}
+              onKeyPress={(event) => {
+                event.key === 'Enter' && sendMessage();
+              }} />
+            <button onClick={sendMessage} >&#9658;</button>
+          </div>
+        </div>
       </div>
-      <div className='chat-body'>
-        <ScrollToBottom className='message-container'>
-          {
-            messageList.map((messageBody, i) => {
-              return <div key={i} className='message' id={googleUser.username === messageBody.author ? 'you' : 'other'}>
-                <div>
-                  <div className='message-meta'>
-                    <img width={'100%'} src={`${messageData.thumbnail}`} alt="profile pic" />
-                    <p id='author'>{messageBody.author}</p>
-                  </div>
-                  <div className='message-content'>
-                    <p>{messageBody.message}</p>
-                    {/* <p id='time'>{messageBody.time}</p> */}
-                  </div>
-                </div>
-              </div>;
-            })
-          }
-        </ScrollToBottom>
-      </div>
-      <div className='chat-footer'>
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder='Type Message'
-          onChange={(event) => setCurrentMessage(event.target.value)}
-          onKeyPress={(event) => {
-            event.key === 'Enter' && sendMessage();
-          }} />
-        <button onClick={sendMessage} >&#9658;</button>
-      </div>
-    </div>
+    </Paper>
   );
 };
 
