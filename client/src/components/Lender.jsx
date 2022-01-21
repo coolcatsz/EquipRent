a505c6280b73c27be0d87c3e953dd4ce1b32bb94
zoobooks <zblaise23@gmail.com>
2022-01-21 03:00:55 -0600

    added border radius to card, color, shadows

--- client/src/components/Lender.jsx
+++ client/src/components/Lender.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { Paper, Typography, Button, TextField, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
 const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/coolcatz/image/upload';
 const CLOUDINARY_UPLOAD_PRESET = 'ny4zarxq';
-import { Paper, Typography, Button, TextField, Grid } from '@mui/material';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
 
 const Lender = ({ user, getAllItem }) => {
   const { register, handleSubmit } = useForm();
@@ -33,10 +34,10 @@ const Lender = ({ user, getAllItem }) => {
       }).then(() => {
         notify();
       }).then(() => {
-        console.log('hiiiiii!');
         navigate('/');
       });
   };
+
   const listItem = data => { 
     axios.post('item/newItems', {
       brand: data.brand,
@@ -72,24 +73,84 @@ const Lender = ({ user, getAllItem }) => {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      style={{ height: '100%', marginTop: '150px', position: 'relative' }}
+      style={{ height: '100%', marginTop: '80px', position: 'relative' }}>
+      <div style={{display: 'inline-block', height: '100vh', }}>
 
-    >
-      <div style={{display: 'inline-block', height: '100vh'}}>
-        <Paper >
-          <Typography align="center" variant="h2">List an Item</Typography>
+        <Paper style={{border: '1px solid grey', borderRadius: '20px', boxShadow: '5px 9px 16px -11px rgba(0,0,0,0.97)'}}>
+          
+          <Typography 
+            paddingTop='15px' 
+            align="center" 
+            variant="h2"
+            fontFamily='Roboto'>
+            List an Item
+          </Typography>
+
           <form onSubmit={handleSubmit(listItem)}>
             <div style={{ padding: '10px' }} spacing={5}>
-   
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Item For Rent" variant="outlined" input {...register('brand')}/></div>
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Category" variant="outlined" input {...register('type')}/></div>
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Item Description" variant="outlined" input {...register('description')}/></div>
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Rental Fee" variant="outlined" type='number' input {...register('price')}/></div>
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Accessed Value" variant="outlined" type='number' input {...register('value')}/></div>
-              <div style={{ padding: '6px' }}><TextField id="outlined-basic" fullWidth label="Condition" variant="outlined" input {...register('condition')}/></div>
-              <div style={{ padding: '6px' }}><label>Upload an image of the item</label><input {...register('itemImg')} type="file"/></div>
-              <div style={{ padding: '6px' }}><Button type="submit" fullWidth variant="contained" style={{padding: '10px'}} color="success">Upload Item</Button></div>
-           
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" 
+                  fullWidth label="Item For Rent" 
+                  variant="outlined" 
+                  input {...register('brand')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" 
+                  fullWidth label="Category" 
+                  variant="outlined" 
+                  input {...register('type')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" 
+                  fullWidth label="Item Description" 
+                  variant="outlined" 
+                  input {...register('description')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" 
+                  fullWidth label="Rental Fee" 
+                  variant="outlined" 
+                  type='number' 
+                  input {...register('price')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" fullWidth 
+                  label="Accessed Value" 
+                  variant="outlined" 
+                  type='number' 
+                  input {...register('value')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <TextField id="outlined-basic" 
+                  color="success" 
+                  fullWidth label="Condition" 
+                  variant="outlined" 
+                  input {...register('condition')}/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <label>Upload an image of the item </label>
+                <input {...register('itemImg')} type="file"/>
+              </div>
+
+              <div style={{ padding: '6px' }}>
+                <Button type="submit" 
+                  fullWidth variant="contained" 
+                  style={{padding: '10px', borderRadius: '10px'}} 
+                  color="success">
+                  Upload Item</Button>
+              </div>
             </div>
           </form>
         </Paper>
