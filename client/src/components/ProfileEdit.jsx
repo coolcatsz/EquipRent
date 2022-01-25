c2a55ba2e7475c6a99a0d39940a76041c0a6c9e2
csherpa <chhoti0712@gmail.com>
2022-01-24 22:07:56 -0600

    setting up a edit profit modal

--- client/src/components/ProfileEdit.jsx
+++ client/src/components/ProfileEdit.jsx
@@ -1,13 +1,60 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const ProfileEdit = ({authUser}) => {
   const {userId} = useParams();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   if (Number(userId) === authUser.id) {
     return (
       <div>
-        <Button>Edit Profile</Button>
+        <Button variant="outlined" onClick={handleClickOpen}>Edit Profile</Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Update Profile</DialogTitle>
+          <form id="my-form-id" >
+            <DialogContent>
+              <Box
+                sx={{
+                  '& > legend': { mt: 2 },
+                }}
+              />
+              <Typography component="legend">About</Typography>
+              <TextField
+                id="filled-multiline-static"
+                label=""
+                multiline
+                rows={4}
+                variant="filled"
+                autoFocus
+                margin="dense"
+                fullWidth
+                // value={description}
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button type="submit" form="my-form-id" onClick={handleClose}>Post</Button>
+            </DialogActions>
+          </form>
+        </Dialog>
       </div>
     );
   } else {
